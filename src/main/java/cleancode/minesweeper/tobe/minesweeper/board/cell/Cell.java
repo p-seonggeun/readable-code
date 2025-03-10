19f4dd1414ecb707be5d8ac8f3d8cf1e689306aa
psg <1999byh@naver.com>
2025-03-10 16:02:02 +0900

    [5-3] 패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/Cell.java
@@ -0,0 +1,22 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public interface Cell {
+
+
+
+    boolean isLandMine();
+
+    boolean hasLandMineCount();
+
+    CellSnapshot getSnapshot();
+
+    void flag();
+
+    void open();
+
+    boolean isChecked();
+
+    boolean isOpened();
+
+    boolean isFlagged();
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
@@ -0,0 +1,60 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public class NumberCell implements Cell {
+
+    private final int nearByLandMineCount;
+    private final CellState cellState = CellState.initialize();
+
+    public NumberCell(int nearByLandMineCount) {
+        this.nearByLandMineCount = nearByLandMineCount;
+    }
+
+    @Override
+    public boolean isLandMine() {
+        return false;
+    }
+
+    @Override
+    public boolean hasLandMineCount() {
+        return true;
+    }
+
+    @Override
+    public CellSnapshot getSnapshot() {
+        if (cellState.isOpened()) {
+            return CellSnapshot.ofNumber(nearByLandMineCount);
+        }
+
+        if (cellState.isFlagged()) {
+            return CellSnapshot.ofFlag();
+        }
+
+        return CellSnapshot.ofUnchecked();
+    }
+
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
+    }
+
+    @Override
+    public boolean isFlagged() {
+        return cellState.isFlagged();
+    }
+}
